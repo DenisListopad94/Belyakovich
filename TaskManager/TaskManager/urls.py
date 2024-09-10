14c71e5a83ca7ff5f565af587f3e3fe5a900ae10
bossniger <egorbelyakovich2@gmail.com>
2024-09-10 15:23:20 +0300

    Django_2 homework

--- TaskManager/TaskManager/urls.py
+++ TaskManager/TaskManager/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from TaskManager.base import views
+from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('tasks/', views.tasks, name='tasks'),
+    path('users/', views.users, name='users'),
 ]
--- TaskManager/base/views.py
+++ TaskManager/base/views.py
@@ -7,4 +7,21 @@ def home(request):
 
 
 def tasks(request):
-    return HttpResponse("Here you will see all your tasks")
+    tasks = [
+        {'task': 'task 1', 'status': 'started', 'category': 'work'},
+        {'task': 'task 2', 'status': 'started', 'category': 'personal'},
+        {'task': 'task 3', 'status': 'completed', 'category': 'work'},
+        {'task': 'task 4', 'status': 'pending', 'category': 'personal'},
+        {'task': 'task 5', 'status': 'started', 'category': 'work'},
+    ]
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+
+def users(request):
+    users = [
+        {"name": "John", "age": 25, "phone": "123-456-7890", 'photo': 'john.jpg'},
+        {"name": "Alice", "age": 30, "phone": "987-654-3210", 'photo': 'static/john.jpg'},
+        {"name": "Bob", "age": 28, "phone": "555-123-4567", 'photo': 'static/john.jpg'},
+        {"name": "Eva", "age": 22, "phone": "666-888-9999", 'photo': 'static/john.jpg'}
+    ]
+    return render(request, 'users.html', {'users': users})
